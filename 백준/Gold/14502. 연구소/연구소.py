2763ecb2e8afc7076312cfc5336f61ab1409598c
최세연 (Lena) <choi20014830@gmail.com>
2024-06-25 21:35:16 +0900

    [Gold IV] Title: 연구소, Time: 760 ms, Memory: 118180 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -13,24 +13,19 @@ result = 0
 
 # 0은 빈 칸, 1은 벽, 2는 바이러스
 
-# 빈칸 찾기와 바이러스 위치 찾기
-empty_spots = []
-virus_positions = []
+# 빈칸 찾기
+empty_spot = []
 for i in range(N):
     for j in range(M):
         if map_list[i][j] == 0:
-            empty_spots.append((i, j))
-        elif map_list[i][j] == 2:
-            virus_positions.append((i, j))
+            empty_spot.append([i, j])
 
 # bfs
-def bfs(graph):
-    q = deque(virus_positions)
-    visited = [[False] * M for _ in range(N)]
+def bfs(i, j, visited, graph):
+    q = deque()
+    q.append((i, j))
+    visited[i][j] = True
     
-    for vx, vy in virus_positions:
-        visited[vx][vy] = True
-
     dx = [0, 0, -1, 1]
     dy = [1, -1, 0, 0]
     
@@ -38,11 +33,16 @@ def bfs(graph):
         x, y = q.popleft()
         for d in range(4):
             nx, ny = x + dx[d], y + dy[d]
+            # print(nx, ny)
             if 0 <= nx < N and 0 <= ny < M:
+                if graph[nx][ny] == 1:
+                    continue
                 if not visited[nx][ny] and graph[nx][ny] == 0:
                     visited[nx][ny] = True
                     graph[nx][ny] = 2
                     q.append((nx, ny))
+                
+    return visited, graph
 
 # 안전 영역 체크
 def safe_check(graph):
@@ -53,17 +53,17 @@ def safe_check(graph):
                 cnt += 1
     return cnt
 
-for spot in combinations(empty_spots, 3):
+for spot in combinations(empty_spot, 3):
+    visited = [[False] * M for _ in range(N)]
     graph = copy.deepcopy(map_list)
     for x, y in spot:
         graph[x][y] = 1
-    bfs(graph)
+    for i in range(N):
+        for j in range(M):
+            if not visited[i][j] and graph[i][j] == 2:
+                visited, graph = bfs(i, j, visited, graph)
     cnt = safe_check(graph)
     result = max(cnt, result)
 
-    # 최대 가능한 안전 영역을 찾으면 조기 종료
-    if result == len(empty_spots) - 3:
-        break
-    
 # 최댓값 출력
 print(result)
